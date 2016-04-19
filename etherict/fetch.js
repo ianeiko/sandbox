18207968b1740f1425dfc90cdfffd32424787204
Oleg Janeiko <ojaneiko@apple.com>
2016-04-18 22:25:57 -0700

    fetch

--- etherict/fetch.js
+++ etherict/fetch.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var colors = require('colors');
+var fs = require('fs');
 
 var MINUTES_5 = 5 * 60;
 var HOURS_24 = 24 * 60 * 60;
@@ -9,17 +9,22 @@ var start_date = Math.round(new Date() / 1000) - HOURS_24; // 24 hours ago
 var period = MINUTES_5;
 var api_base_url = `https://poloniex.com/public?command=returnChartData&currencyPair=${currency_pair}&end=9999999999&period=${period}`;
 
+var DATA_DIR = 'data';
+var DATA_DEST = `${DATA_DIR}/${currency_pair}.json`;
+
 function fetch_data() {
   var api_url = api_base_url + `&start=${start_date}`;
   request(api_url, (err, response, body) => {
-    if(err) return err;
+    if(err) throw err;
 
-    body = JSON.parse(body);
-    console.log(api_url);
+    var parsedBody = JSON.parse(body);
+    if(parsedBody.length <= 0) return;
 
-    console.log(body.length);
-    console.log(body[0]);
-    console.log(body[body.length - 1]);
+    fs.writeFile(DATA_DEST, body, (err) => {
+      if(err) throw err;
+
+      console.log(`Added ${parsedBody.length} records to ${DATA_DEST}`);
+    });
   });
 }
 
